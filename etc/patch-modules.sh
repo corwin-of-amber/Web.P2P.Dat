6a8d16352b0611636afba908b439378171a9768e
Shachar Itzhaky <corwin.amber@gmail.com>
2019-07-10 01:13:05 +0300

    [node] Patch for running discovery in node as well.

--- etc/patch-modules.sh
+++ etc/patch-modules.sh
@@ -1,1 +1,5 @@
+# Update simple-peer inside webrtc-swarm to latest
 cp /tmp/node_modules/simple-peer/index.js node_modules/webrtc-swarm/node_modules/simple-peer/index.js
+
+# discovery-swarm-web
+patch -N -p1 -d node_modules/discovery-swarm-web < etc/patches/discovery-swarm-web.patch || true
