1a2f651ca4118c07f8c9302e4c54ef53eff95bca
Shachar Itzhaky <corwin.amber@gmail.com>
2019-08-26 18:58:52 +0300

    [refactor] Introduce FirepadTreeMerge data type.
    Used to merge edits from multiple clients, effectively providing
    a text CRDT alternative to Automerge.Text.

---
+++ src/addons/firepad-conflow.js
@@ -0,0 +1,210 @@
+const _ = require('lodash'),
+      assert = require('assert'),
+      {TextOperation} = require('firepad-core');
+
+
+
+/**
+ * Firepad tree merging logic.
+ */
+class FirepadTreeMerge {
+
+    constructor() {
+        this.operations = []
+        this.applied = new Map();
+        this.residues = new Map();
+        this.ownIds = new Set();
+    }
+
+    newOperation(operation, id) {
+        var tip = this.operations.slice(-1)[0],
+            parentLink = tip && [tip.id, tip.v.length - 1],
+            e = {id, v:[{o:operation, p:parentLink}]};
+        return e;
+    }
+
+    push(operation) {
+        this.operations.push(operation);
+        this.applied.set(operation.id, operation.v[0].o);
+        this.ownIds.add(operation.id);
+    }
+
+    insert(index, operation) {
+        let incoming = operation.v.slice(-1)[0],
+            [residue, adj] = this.preadjust(index, incoming),
+            ret = this.postadjust(index, adj);
+        this.operations.splice(index, 0, operation);
+        this.applied.set(operation.id, adj);
+        this.residues.set(operation.id, residue);
+        return ret;
+    }
+
+    preadjust(index, incoming) {
+        var acc = incoming.o, pindex = -1, residue;
+
+        /* "Vertical" */
+        if (incoming.p) {
+            let [pid, pa] = incoming.p;
+            pindex = this.operations.findIndex(e => e.id === pid);
+            assert(pindex >= 0, 'parent operation is missing');
+            let pel = this.operations[pindex];
+            assert(pel.v.length > pa, 'parent operation is too new');
+            for (let rev of pel.v.slice(pa + 1))
+                acc = this._xform(rev.r, acc)[1];  /* [0] is ignored */
+        
+            residue = this.residues.get(pid);
+            if (residue) {
+                [residue, acc] = this._xform(residue, acc);
+            }
+        }
+
+        /* "Horizontal" */
+        for (let el of this.operations.slice(pindex + 1, index)) {
+            let x = this._xform(this.applied.get(el.id), acc);
+            residue = this._compose(residue, x[0]);
+            acc = x[1];
+        }
+        return [residue, acc];
+    }
+
+    postadjust(index, incoming) {
+        var acc = incoming;
+        for (let el of this.operations.slice(index)) {
+            let x = this._xform(acc, this.applied.get(el.id));
+            this.residues.set(el.id, this._compose(this.residues.get(el.id), x[0]));
+            this.applied.set(el.id, x[1]);
+            acc = x[0];
+        }
+        return acc;
+    }
+
+    /**
+     * Writes applied versions as new revisions of owned operations,
+     * as long as such operations reside in a linear prefix.
+     * @param {Set} opIds 
+     */
+    rebase(opIds=this.ownIds) {
+        var lastId = undefined, revised = [];
+        for (let [index, el] of this.operations.entries()) {
+            if (opIds.has(el.id)) {
+                let residue = this.residues.get(el.id);
+                if (residue) {
+                    let nextRev = {o:this.applied.get(el.id), p:lastId, r:residue};
+                    el.v.push(nextRev);
+                    this.residues.delete(el.id);
+                    revised.push([index, nextRev]);
+                }
+            }
+
+            let top = el.v.slice(-1)[0];
+            if (!_.isEqual(top.p, lastId)) break; /* stops being linear */
+
+            lastId = [el.id, el.v.length - 1];
+        }
+
+        return revised;
+    }
+    
+    /**
+     * Incorporates the results of rebases from other writers.
+     */
+    rebased(index, operation) {
+        var incumbent = this.operations[index];
+        assert.equal(incumbent.id, operation.id);
+        assert.equal(JSON.stringify(incumbent.v),
+                     JSON.stringify(operation.v.slice(0, incumbent.v.length)));
+
+        this.operations[index] = operation;
+
+        var [residue, adj] = this.preadjust(index, operation.v.slice(-1)[0]);
+        this.residues.set(operation.id, residue);
+
+        assert.equal(JSON.stringify(adj), JSON.stringify(this.applied.get(operation.id)));
+    }
+
+    isLinear() {
+        var lastId = undefined;
+        for (let el of this.operations) {
+            let top = el.v.slice(-1)[0];
+            if (!_.isEqual(top.p, lastId)) return false;
+            lastId = [el.id, el.v.length - 1];
+        }
+        return true;
+    }
+
+    get raw() {
+        return this.operations.map(el => el.v.slice(-1)[0].o);
+    }
+    get seq() {
+        return this.operations.map(el => this.applied.get(el.id));
+    }
+
+
+    _xform(o1, o2) {
+        return this._op(o1).transform(this._op(o2));
+    }
+
+    _compose(o1, o2) {
+        if (!o1) return o2;
+        if (!o2) return o1;
+        return this._op(o1).compose(this._op(o2));
+    }
+
+    _op(o) {
+        return (o instanceof TextOperation) ? o : TextOperation.fromJSON(o);
+    }
+
+    _byId(objs) {
+        var d = new Map();
+        for (let o of objs)
+            d.set(o.id, o);
+        return d;
+    }
+
+
+    /**
+     * This is here for now as "specification" guidelines.
+     * @param {*} operationTree 
+     */
+    _flatten(operationTree) {
+        var flat = [], residues = []; //, acc = [];// operationTree.slice(0, 1);
+        for (let el of operationTree) {
+            /*
+            var incoming = el.incoming;
+
+            if (!incoming) {
+                acc = this._xform(acc, el.o)[0];
+            }
+            */
+            var pindex = flat.findIndex(e => e.id === el.p),
+                residue = residues[pindex];
+            if (residue) {
+                let x = this._xform(residue, el.o);
+                residue = x[0];
+                console.log('residue', JSON.stringify(residue));
+                el = {id: el.id, o: x[1], p: el.p};
+            }
+            for (let le of flat.slice(pindex + 1)) {
+                let x = this._xform(le.o, el.o);
+                residue = residue ? this._compose(residue, x[0]) : x[0];
+                console.log('residue', JSON.stringify(residue));
+                el = {id: el.id, o: x[1], p: le.id};
+            }
+            flat.push(el);
+            residues.push(residue);
+            /*
+            if (incoming) {
+                acc = this._compose(acc, el.o)
+            }
+
+            console.log(JSON.stringify(acc));
+            */
+        }
+        return flat;
+    }
+
+}
+
+
+
+module.exports = {FirepadTreeMerge};
--- src/ui/syncpad.js
+++ src/ui/syncpad.js
@@ -1,6 +1,9 @@
-const {FirepadCore, TextOperation} = require('firepad-core');
+const _ = require('lodash'),
+      assert = require('assert');
 
-const _ = require('lodash');
+const {FirepadCore} = require('firepad-core'),
+      {FirepadTreeMerge} = require('../addons/firepad-conflow');
+
 
 
 
@@ -16,7 +19,10 @@ class SyncPad {
         this.editor = editor;
         this.slot = slot;
 
-        this.firepad = new FirepadCore(editor, opts);
+        this.firepad = new FirepadCore(this.editor, opts);
+        this.tm = new FirepadTreeMerge();
+
+        this._outbox = undefined;
 
         (this._park = slot.park()).then(() => this._formLink());
     }
@@ -27,6 +33,10 @@ class SyncPad {
         if (this.amFlush)   this.editor.off('beforeChange', this.amFlush);
         if (this.amHandler) this.amHandler.unregister();
         this.firepad.dispose();
+    }
+
+    get ready() {
+        return this._park || Promise.resolve();
     }
 
     _formLink() {
@@ -40,48 +50,125 @@ class SyncPad {
 
         this.editor.setValue('');
 
-        for (let op of obj.operations)
-            this._pull(op);
-
         // Firepad -> Automerge
         this.fpHandler = (data) => {
-            if (data.operation)
-                this._push(data.operation);
+            if (data.operation) {
+                assert(!this._outbox);
+                
+                //let userId = this.firepad.serverAdapter.userId_;
+                //console.log('operation', userId, JSON.stringify(data.operation));
+
+                data.preventDefault(); // postpone 'ack'
+                this._outbox = data;
+                process.nextTick(() => {
+                    debouncedPatch.flush();
+                    this._maybeAccept();
+                });
+            }
         };
         this.firepad.on('data', this.fpHandler);
 
-        this.editor.on('beforeChange', this.amFlush =
-            () => debouncedPatch.flush());
-        
         // Automerge -> Firepad
         this.amHandler = registerHandlerObj(subslots.operations, 
             (newVal, newRev, oldRev, changes) => {
                 var diff = patchFromChanges(oldRev, changes);
-                debouncedPatch(diff);
+                debouncedPatch(newVal, diff);
+                //debouncedPatch.flush();
             });
         
-        const debouncedPatch = debounceQueue((diff) => {
+        const debouncedPatch = debounceQueue((values, diff) => {
+            //let userId = this.firepad.serverAdapter.userId_;
             for (let entry of diff) {
-                if (entry.action === 'insert')
-                    this._pull(entry.value);
-                // TODO transform operation if not at end!
+                if (entry.action === 'insert') {
+                    //console.log(entry.index, entry.value, `(${userId})`);
+                    var operation = {id: entry.elemId, v: JSON.parse(entry.value)};
+                    operation = this.tm.insert(entry.index, operation);
+                    this.firepad.data({operation});
+                    this._maybeReject();
+                }
+                else if (entry.action === 'set') {
+                    //console.log(entry.index, '-', entry.value, `(${userId})`);
+                    var elemId = automerge.Frontend.getElementIds(values)[entry.index],
+                        operation = {id: elemId, v: JSON.parse(entry.value)};
+                    this.tm.rebased(entry.index, operation)
+                }
             }
-        }, 50, {maxWait: 500});
+        }, 50, {maxWait: 500, afterFlush: () => {
+            this._rebase();
+            this._maybeAccept();
+        } });
+
+        this._populate(obj.operations);
     }
 
-    _push(operation) {
-        this.slot.change(o => o.operations.push(JSON.stringify(operation)),
-            newRev => { this.amHandler.skipFwd(newRev); });
+    _withIds(values) {
+        return _.zip(values, automerge.Frontend.getElementIds(values))
+                .map(([value,id]) => ({id, v:JSON.parse(value)}));
     }
 
-    _pull(value) {
-        this.firepad.data({operation: this._asOperation(value)});
+    _tip(values) {
+        values = values || this.slot.get().operations;
+        assert(values.length > 0);
+        return automerge.Frontend.getElementIds(values).slice(-1)[0];
     }
 
-    _asOperation(value) {
-        return TextOperation.fromJSON(JSON.parse(value));
+    _push(operation, accept) {
+        var reified = this.tm.newOperation(operation);
+        reified.v[0].a = this.firepad.serverAdapter.userId_;
+        //console.log('push', JSON.stringify(reified.v));
+        this.slot.change(o => o.operations.push(JSON.stringify(reified.v)),
+                         newRev => {
+                             this.amHandler.skipFwd(newRev); 
+                             reified.id = this._tip(this.slot.getFrom(newRev).operations);
+                             this.tm.push(reified);
+                             accept();
+                             //this._trace(newRev);
+                         });
     }
 
+    _trace(rev, msg='revision') {
+        var userId = this.firepad.serverAdapter.userId_;
+        console.warn(msg, userId);
+        for (let o of rev.firepad.operations) {
+            o = o && JSON.parse(o)[0]; o = o && {o:o.o, a:o.a};
+            console.log(`${JSON.stringify(o)}`);
+        }
+    }
+
+    _rebase() {
+        this.slot.change(o => {
+            for (let [index, newRev] of this.tm.rebase()) {
+                let v = JSON.parse(o.operations[index]);
+                v.push(newRev);
+                o.operations[index] = JSON.stringify(v);
+            }
+        }, newRev => { this.amHandler.skipFwd(newRev); });
+    }
+
+    _maybeAccept() {
+        if (this._outbox && this.tm.isLinear()) {
+            var data = this._outbox;
+            this._outbox = undefined;
+            this._push(data.operation, () => data.accept());
+        }
+    }
+
+    _maybeReject() {
+        if (this._outbox) {
+            var data = this._outbox;
+            this._outbox = undefined;
+            data.reject();
+        }
+    }
+
+    _populate(operations) {
+        assert(this.tm.operations.length === 0);
+
+        for (let [index, entry] of this._withIds(operations).entries()) {
+            let operation = this.tm.insert(index, entry);
+            this.firepad.data({operation});
+        }
+    }
 }
 
 
@@ -106,11 +193,13 @@ class FirepadShare {
 function registerHandlerWithChanges(docSlot, handler) {
     var lastRev = docSlot.get() || automerge.init(), h;
     docSlot.registerHandler(h = newRev => {
-        var changes = automerge.getChanges(lastRev, newRev);
-        try {
-            handler(newRev, newRev, lastRev, changes);
-        }
-        finally { lastRev = newRev; }
+        var changes = automerge.getChanges(lastRev, newRev),
+            p = lastRev;
+        lastRev = newRev;
+        //try {
+            handler(newRev, newRev, p, changes);
+        //}
+        //finally { if (lastRev === p) lastRev = newRev; }
     });
     return {
         unregister() { docSlot.unregisterHandler(h); },
@@ -138,11 +227,16 @@ function patchFromChanges(oldDoc, changes) {
 
 
 function debounceQueue(func, wait, options) {
-    var queue = [];
+    var queue = [], callback = options.afterFlush;
     const kick = _.debounce(() => {
         while (queue.length) {
             func(...queue.shift());
         }
+        // Important: use nextTick here to allow callback to make further
+        // calls to debouncer.flush() without recursing.
+        if (callback) process.nextTick(() => {
+            if (queue.length === 0) callback();
+        });
     }, wait, options);
     function debouncer(...args) {
         queue.push(args); kick();
