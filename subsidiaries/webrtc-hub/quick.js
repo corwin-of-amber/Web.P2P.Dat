f4ec7c9b0bb7e7eb85560f54451b4ecdf9b243cc
Shachar Itzhaky <corwin.amber@gmail.com>
2022-01-01 16:12:45 +0200

    [bugfix] Corrected poke attributes in webrtc-hub.

--- subsidiaries/webrtc-hub/quick.js
+++ subsidiaries/webrtc-hub/quick.js
@@ -9,16 +9,21 @@ var server = new Turn({
   },
   pokeAuthority: 'stun.l.google.com:19302',
   pokeInterval: 600 * 1000,
+  debugLevel: 'TRACE'
 });
 server.start();
 
 server.poke = async function() {
-   const res = await stun.request(this.pokeAuthority),
+    const res = await stun.request(this.poke.authority),
           xaddr = res.getXorAddress();
-    console.log(xaddr.family, xaddr.address);
+    console.log(new Date(), xaddr.family, xaddr.address);
     this.externalIps = xaddr.address;
 };
 
+/* @todo read from config */
+server.poke.authority = 'stun.l.google.com:19302';
+server.poke.interval = 600 * 1000;
+
 server.poke();
-setInterval(() => server.poke(), server.pokeInterval);    
+setInterval(() => server.poke(), server.poke.interval);
 
