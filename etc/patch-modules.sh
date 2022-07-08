284f10add8d3ea79fe75a8bc5bdc39d41149966f
Shachar Itzhaky <corwin.amber@gmail.com>
2022-07-08 20:24:32 +0300

    [cleanup] These patches are anachronistic.

--- etc/patch-modules.sh
+++
@@ -1,8 +0,0 @@
-
-# discovery-swarm-web
-# (allow to run in Node)
-#patch -N -p1 -d node_modules/discovery-swarm-web < etc/patches/discovery-swarm-web.patch || true
-
-# @geut/discovery-swarm-webrtc
-# (force installed webrtc-swarm)
-rm -rf node_modules/@geut/discovery-swarm-webrtc/node_modules/webrtc-swarm
